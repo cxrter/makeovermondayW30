794c3976e0df6bb6b54dbe8289a475f73807ee30
cxrter <68082432+cxrter@users.noreply.github.com>
2020-07-29 13:18:34 +0100

    Update bubbles.js

--- static/js/bubbles.js
+++ static/js/bubbles.js
@@ -1,16 +1,44 @@
 // set the dimensions and margins of the graph
 var margin = {top: 100, right: 30, bottom: 150, left: 50},
-    height = 900 - margin.top - margin.bottom
+    height = 800 - margin.top - margin.bottom
     width = 1400 - margin.left - margin.right
     radius = 5
     padding = 1     // space between nodes
     cluster_padding = 5;    // Space between nodes in different stages
+
+function responsivefy(svg) {
+      // get container + svg aspect ratio
+      var container = d3.select(svg.node().parentNode),
+          width = parseInt(svg.style("width")),
+          height = parseInt(svg.style("height")),
+          aspect = width / height;
+
+      // add viewBox and preserveAspectRatio properties,
+      // and call resize so that svg resizes on inital page load
+      svg.attr("viewBox", "0 0 " + width + " " + height)
+          .attr("preserveAspectRatio", "xMinYMid")
+          .call(resize);
+
+      // to register multiple listeners for same event type,
+      // you need to add namespace, i.e., 'click.foo'
+      // necessary if you call invoke this function for multiple svgs
+      // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+      d3.select(window).on("resize." + container.attr("id"), resize);
+
+      // get width of container and resize svg to fit it
+      function resize() {
+          var targetWidth = parseInt(container.style("width"));
+          svg.attr("width", targetWidth);
+          svg.attr("height", Math.round(targetWidth / aspect));
+      }
+    }
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_viz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .call(responsivefy)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")")
